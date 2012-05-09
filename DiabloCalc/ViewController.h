358897852011fcd5c57c9999ee63420d4b1ac079
John Koerner <jck_osx@Johns-Mac-Pro.local>
2012-05-09 08:47:04 -0500

    Initial Commit

---
+++ DiabloCalc/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DiabloCalc
+//
+//  Created by John Koerner on 5/9/12.
+//  Copyright (c) 2012 Etude Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ DiabloCalc/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DiabloCalc
+//
+//  Created by John Koerner on 5/9/12.
+//  Copyright (c) 2012 Etude Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
